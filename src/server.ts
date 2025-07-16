59dfbfa93f16c9938118feba50939be189c0e04f
humayonitbd <humayonforid44@gmail.com>
2025-07-16 12:04:50 +0600

    update server work

--- src/server.ts
+++ src/server.ts
@@ -10,51 +10,13 @@ import { createSuperAdmin } from './app/DB';
 let server: Server;
 const socketServer = createServer();
 
-// Initialize Socket.IO with type safety
 const io: SocketIOServer = new SocketIOServer(socketServer, {
   cors: {
     origin: '*',
   },
 });
 
-// async function main() {
-//   try {
-//     // // console.log('config.database_url', config.database_url);
-//     // Connect to MongoDB
-//     // await mongoose.connect(config.database_url as string);
-//     await mongoose.connect(
-//       `mongodb://${config.database_user_name}:${config.databse_user_password}@mongo:${config.database_port}/${config.database_name}?authSource=admin`,
-//     );
-//     // await mongoose.connect(
-//     //   'mongodb+srv://tiger:tiger@team-codecanyon.ffrshve.mongodb.net/pro-mentors?retryWrites=true&w=majority&appName=Team-CodeCanyon',
-//     // );
 
-//     // Start Express server
-//     // server = app.listen(Number(config.port), config.ip as string, () => {
-//     server = app.listen(Number(config.port), () => {
-//       console.log(
-//         colors.green(`App is listening on ${config.ip}:${config.port}`).bold,
-//       );
-//     });
-
-//     // Start Socket server
-//     socketServer.listen(config.socket_port || 6000, () => {
-//       console.log(
-//         colors.yellow(
-//           `Socket is listening on ${config.ip}:${config.socket_port}`,
-//         ).bold,
-//       );
-//     });
-
-//     // Pass Socket.IO instance to socketIO module
-//     socketIO(io);
-//     global.io = io;
-//   } catch (err) {
-//     console.error('Error starting the server:', err);
-//     // console.log(err);
-//     process.exit(1); // Exit after error
-//   }
-// }
 
 async function main() {
   try {
