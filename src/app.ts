f06cbd9824153402bc21af5f135b933d603a43ee
humayonitbd <humayonforid44@gmail.com>
2025-07-20 11:17:38 +0600

    update origin

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     // origin: 'https://memorial-moments-website.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
